6c7350bfe1878fa4ac1682762085dbd9b48e4f21
denwaotoko <eurioh316@gmail.com>
2015-05-06 00:11:33 -0700

    denwafighteronline

---
+++ dfol/denwafighteronline.user.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+var images = document.getElementsByTagName('img');
+for (var i = 0; i < images.length; i++) {
+images[i].src = images[i].src.replace('http://www.dfonexus.com/data/avatars/l/0/664.jpg', 'http://a.pomf.se/jqpwic.png');
+images[i].src = images[i].src.replace('http://www.dfonexus.com/data/avatars/l/0/587.jpg', 'http://i2.kym-cdn.com/entries/icons/original/000/001/030/dickbutt.jpg');
+}
+}, false);
+
